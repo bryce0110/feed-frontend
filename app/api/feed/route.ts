7a2e86c95ee94723b2cbb9a0bf2db8785c41aa15
bryce0110 <bryce0110@gmail.com>
2024-11-08 16:02:18 -0600

    Add Simple API Route for DB

---
+++ app/api/feed/route.ts
@@ -0,0 +1,26 @@
+import { apiGet, apiPost } from "../database";
+
+// TODO: Implement POST function
+export async function POST(req: Request, res: Response) {}
+
+export async function GET(req: Request, res: Response) {
+    const query = `
+        SELECT * FROM feed
+    `;
+
+    let status, body;
+    try {
+        await apiGet(query)
+        .then((res) => {
+            status = 200;
+            body = res;
+        })
+        .catch((err) => {
+            status = 400;
+            body = err;
+        });
+        return Response.json(body, { status });
+    } catch (err) {
+        return Response.json(err, { status: 400 });
+    }
+}
